c5ebcbb068d96644a076dfb993224f041b09ea57
Kenneth Gerardo García Lemus <kgarcia-2020050@kinal.edu.gt>
2022-07-11 20:20:44 -0600

    Final

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const app = require("./app");
 const admin = require("./src/controllers/inicio.controller");
 mongoose.Promise = global.Promise;
 mongoose
-  .connect("mongodb+srv://takeru:12345@cluster0.ppw9e.mongodb.net/?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://takeru:12345@cluster0.ppw9e.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
-    app.listen(3000, function () {
-      console.log("Conexion exitosa a la db");
-      console.log("Corriendo en el puerto 3000");
+    const port = process.env.PORT;
+
+    app.listen(port || 3030, function () {
+      return console.log("Corriendo en el puerto " + port);
     });
   })
   .catch((error) => {
