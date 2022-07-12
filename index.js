7ac0bed2c7fd8642148e083ba700fd03fe158876
Kenneth Gerardo García Lemus <kgarcia-2020050@kinal.edu.gt>
2022-07-11 20:16:47 -0600

    Final

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require("./app");
 const admin = require("./src/controllers/inicio.controller");
 mongoose.Promise = global.Promise;
 mongoose
-  .connect("mongodb://localhost:27017/Hoteles", {
+  .connect("mongodb+srv://takeru:12345@cluster0.ppw9e.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
